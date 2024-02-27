db1f63e0e471a84d9499da3f32ca589ba7f57e22
Angel Guillermo de Jesús Pérez Jiménez <140100841+LightDemon12@users.noreply.github.com>
2024-02-26 18:57:12 -0600

    Lector y grafico
    el lector guarda las instrucciones en orden en una lista y se actualiza el estado y todos los atributos cada que se realiza un comando
    
    el grafico se crea en nodos
    
    -faltante
    *elección de ruta archivo
    *archivo result unico (no escribir en el mismo n veces)

--- Mascota.py
+++ Mascota.py
@@ -1,5 +1,5 @@
 import datetime
-from graphviz import Digraph  # Importar la clase Digraph desde la librería Graphviz
+from graphviz import Digraph  
 
 class Mascota:
     def __init__(self, id, nombre, energia=1, tipo="gato", estado="vivo"):
@@ -22,6 +22,7 @@ class MascotaFinal:
 # Función para cargar mascotas desde un archivo
 def cargar_mascotas_desde_archivo(nombre_archivo):
     gatos = []
+    instrucciones = []
     with open(nombre_archivo, 'r', encoding='cp1252') as file:
         lineas = file.readlines()
 
@@ -29,28 +30,45 @@ def cargar_mascotas_desde_archivo(nombre_archivo):
         partes = [parte.strip() for parte in linea.split(':')]
         if len(partes) > 1:
             comando, parametros = partes[0], partes[1]
-            if comando == "Crear_Gato":
-                parametros_divididos = parametros.split(',')
-                nombre_gato = parametros_divididos[0]
-                tipo_gato = parametros_divididos[1].strip() if len(parametros_divididos) > 1 else "gato"
-                estado_gato = parametros_divididos[2].strip() if len(parametros_divididos) > 2 else "vivo"
-                nuevo_gato = Mascota(idx, nombre_gato, tipo=tipo_gato, estado=estado_gato)
-                gatos.append(nuevo_gato)
-                imprimir_mensajes_acciones(nuevo_gato, "Crear_Gato", nuevo_gato.energia, nuevo_gato.energia, "mascotas.petworld_result")
-            elif comando == "Dar_de_Comer":
-                nombre_gato, peso_ratón = parametros.split(',')
-                for gato in gatos:
-                    if gato.nombre == nombre_gato:
-                        energia_antes = gato.energia
-                        gato.energia += 12 + int(peso_ratón)
-                        imprimir_mensajes_acciones(gato, "Dar_de_Comer", energia_antes, gato.energia, "mascotas.petworld_result")
-            elif comando == "Jugar":
-                nombre_gato, tiempo = parametros.split(',')
-                for gato in gatos:
-                    if gato.nombre == nombre_gato:
-                        energia_antes = gato.energia
-                        gato.energia -= int(tiempo) // 2  # Corregido aquí
-                        imprimir_mensajes_acciones(gato, "Jugar", energia_antes, gato.energia, "mascotas.petworld_result")
+            instrucciones.append((comando, parametros))
+
+    for idx, (comando, parametros) in enumerate(instrucciones, start=1):
+        if comando == "Crear_Gato":
+            parametros_divididos = parametros.split(',')
+            nombre_gato = parametros_divididos[0]
+            tipo_gato = parametros_divididos[1].strip() if len(parametros_divididos) > 1 else "gato"
+            estado_gato = parametros_divididos[2].strip() if len(parametros_divididos) > 2 else "vivo"
+            nuevo_gato = Mascota(idx, nombre_gato, tipo=tipo_gato, estado=estado_gato)
+            gatos.append(nuevo_gato)
+            imprimir_mensajes_acciones(nuevo_gato, "Crear_Gato", nuevo_gato.energia, nuevo_gato.energia, "mascotas.petworld_result")
+        elif comando == "Dar_de_Comer":
+            nombre_gato, peso_ratón = parametros.split(',')
+            for gato in gatos:
+                if gato.nombre == nombre_gato and gato.estado != "muerto":
+                    energia_antes = gato.energia
+                    gato.energia += 12 + int(peso_ratón)
+                    imprimir_mensajes_acciones(gato, "Dar_de_Comer", energia_antes, gato.energia, "mascotas.petworld_result")
+                    break  # Salir del bucle una vez que se encuentra la mascota
+        elif comando == "Jugar":
+            nombre_gato, tiempo = parametros.split(',')
+            for gato in gatos:
+                if gato.nombre == nombre_gato and gato.estado != "muerto":
+                    energia_antes = gato.energia
+                    gato.energia -= int(tiempo) // 2
+                    imprimir_mensajes_acciones(gato, "Jugar", energia_antes, gato.energia, "mascotas.petworld_result")
+                    if gato.energia <= 0:
+                        gato.estado = "muerto"
+                        imprimir_mensajes_acciones(gato, "Muerte", energia_antes, gato.energia, "mascotas.petworld_result")
+                    break  # Salir del bucle una vez que se encuentra la mascota
+        elif comando == "Resumen_Mascota":
+            nombre_gato = parametros.strip()
+            for gato in gatos:
+                if gato.nombre == nombre_gato:
+                    print(gato)
+        elif comando == "Resumen_Global":
+            for gato in gatos:
+                print(gato)
+
     return gatos
 
 # Función para generar el gráfico PNG
@@ -58,54 +76,50 @@ def generar_grafico(mascotas_finales):
     dot = Digraph(comment='Mascotas')
 
     for mascota in mascotas_finales:
-        dot.node(f'{mascota.nombre}', f'Nombre: {mascota.nombre}\nEnergía Final: {mascota.energia_final}\nTipo: {mascota.tipo}\nEstado: {mascota.estado}')
+        # Nodo central con el nombre del gato
+        dot.node(f'{mascota.nombre}', f'Nombre: {mascota.nombre}')
+
+        # Nodos para la energía, estado y tipo, apuntando al nodo central
+        dot.node(f'{mascota.nombre}_energia', f'Energía: {mascota.energia_final}')
+        dot.node(f'{mascota.nombre}_estado', f'Estado: {mascota.estado}')
+        dot.node(f'{mascota.nombre}_tipo', f'Tipo: {mascota.tipo}')
+
+        dot.edge(f'{mascota.nombre}', f'{mascota.nombre}_energia')
+        dot.edge(f'{mascota.nombre}', f'{mascota.nombre}_estado')
+        dot.edge(f'{mascota.nombre}', f'{mascota.nombre}_tipo')
 
     dot.render('mascotas', format='png', cleanup=True)
+
 
 # Función para imprimir mensajes de acciones
 def imprimir_mensajes_acciones(gato, accion, energia_antes, energia_despues, output_file=None):
     now = datetime.datetime.now()
 
     if output_file:
-        with open(output_file, 'a', encoding='utf-8') as file:
+        with open(output_file, 'a+', encoding='utf-8') as file:
             if accion == "Crear_Gato":
-                file.write("*" * 50 + "\n")
                 file.write(f"[{now}] Se creó el gato {gato.nombre}\n")
             elif accion == "Dar_de_Comer":
                 file.write(f"[{now}] le diste de comer a {gato.nombre}, su energia: Antes: {energia_antes}, Ahora: {energia_despues}\n")
             elif accion == "Jugar":
                 file.write(f"[{now}] Jugaste con {gato.nombre}, su energia: Antes: {energia_antes}, Ahora: {energia_despues}\n")
-                if energia_despues <= 0:
-                    gato.estado = "muerto"
-                    file.write(f"[{now}] El gato {gato.nombre} ha muerto debido a la falta de energía.\n")
-                    file.write("[{}] Chales ya me morí\n".format(now))
-                    file.write("\n--------------Resumen Global--------------------------\n")
-                    file.write(f"{gato.nombre}, {gato.energia}, {gato.tipo}, {gato.estado}\n")
-                    file.write("*" * 50 + "\n")
-                else:
-                    file.write("\n--------------Resumen Global--------------------------\n")
-                    file.write(f"{gato.nombre}, {gato.energia}, {gato.tipo}, {gato.estado}\n")
-                    file.write("*" * 50 + "\n")
+            elif accion == "Muerte":
+                file.write(f"[{now}] El gato {gato.nombre} ha muerto debido a la falta de energía.\n")
+                file.write("[{}] Chales ya me morí\n".format(now))
+                file.write(f"{gato.nombre}, {gato.energia}, {gato.tipo}, {gato.estado}\n")
+ 
 
-    # Ahora también imprimimos en la consola
     if accion == "Crear_Gato":
-        print("*" * 50)  # Separador antes de crear un nuevo gato
         print(f"[{now}] Se creó el gato {gato.nombre}")
     elif accion == "Dar_de_Comer":
         print(f"[{now}] le diste de comer a {gato.nombre}, su energia: Antes: {energia_antes}, Ahora: {energia_despues}")
     elif accion == "Jugar":
         print(f"[{now}] Jugaste con {gato.nombre}, su energia: Antes: {energia_antes}, Ahora: {energia_despues}")
-        if energia_despues <= 0:
-            gato.estado = "muerto"
-            print(f"[{now}] El gato {gato.nombre} ha muerto debido a la falta de energía.")
-            print("[{}] Chales ya me morí".format(now))
-            print("\n--------------Resumen Global--------------------------")
-            print(f"{gato.nombre}, {gato.energia}, {gato.tipo}, {gato.estado}")
-            print("*" * 50)  # Separador solo después de jugar
-        else:
-            print("\n--------------Resumen Global--------------------------")
-            print(f"{gato.nombre}, {gato.energia}, {gato.tipo}, {gato.estado}")
-            print("*" * 50)  # Separador solo después de jugar
+    elif accion == "Muerte":
+        print(f"[{now}] El gato {gato.nombre} ha muerto debido a la falta de energía.")
+        print("[{}] Chales ya me morí".format(now))
+        print(f"{gato.nombre}, {gato.energia}, {gato.tipo}, {gato.estado}")
+
 
 # Función para el menú principal
 def menu_principal():
@@ -114,7 +128,7 @@ def menu_principal():
     input()
     menu_opciones()
 
-# Función para el menú de opciones
+
 def menu_opciones():
     while True:
         print("\n--- MENÚ ---")
@@ -123,12 +137,10 @@ def menu_opciones():
         opcion = input("Elige una opción: ")
         if opcion == "1":
             gatos = cargar_mascotas_desde_archivo("mascotas.petmanager")
-            # Crear la lista de mascotas finales para generar el gráfico
             mascotas_finales = []
             for gato in gatos:
                 mascota_final = MascotaFinal(gato.nombre, gato.energia, gato.tipo, gato.estado)
                 mascotas_finales.append(mascota_final)
-            # Generar el gráfico PNG
             generar_grafico(mascotas_finales)
         elif opcion == "2":
             print("Saliendo del programa...")
